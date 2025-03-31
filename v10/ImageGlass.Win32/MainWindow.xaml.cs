fdf0b9429ce8e4bc2347db35b329b11efe10304b
Phap Dieu Duong <ddphap@gmail.com>
2025-03-31 20:10:02 +0800

    Added Grid layout

--- v10/ImageGlass.Win32/MainWindow.xaml.cs
+++ v10/ImageGlass.Win32/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
-﻿using Microsoft.UI;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+﻿using Microsoft.UI.Xaml;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -32,9 +19,16 @@ public sealed partial class MainWindow : Window
         AppWindow.TitleBar.PreferredTheme = Microsoft.UI.Windowing.TitleBarTheme.UseDefaultAppMode;
 
         Title = $".NET {Environment.Version} - {Environment.OSVersion}";
+
+
+        ExtendsContentIntoTitleBar = true;
+        SetTitleBar(WinMainTitleBar);
     }
 
-
+    public GridLength TitleBarLeftInset => new(AppWindow.TitleBar.LeftInset);
+    public GridLength TitleBarRightInset => new(AppWindow.TitleBar.RightInset);
+    public GridLength TitleBarHeight => new(AppWindow.TitleBar.Height / 2.5f);
+    public Thickness TitleBarMargin => new Thickness(0, 0, AppWindow.TitleBar.RightInset / 2.5f, 0);
 
 
     private void Window_Closed(object sender, WindowEventArgs args)
