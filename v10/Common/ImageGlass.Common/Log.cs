449d66002d673e75856e08ef81369352cdde74fe
Phap Dieu Duong <ddphap@gmail.com>
2025-04-07 17:20:43 +0800

    Create Log.cs

---
+++ v10/Common/ImageGlass.Common/Log.cs
@@ -0,0 +1,47 @@
+﻿/*
+ImageGlass Project - Image viewer for Windows
+Copyright (C) 2010 - 2025 DUONG DIEU PHAP
+Project homepage: https://imageglass.org
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Diagnostics;
+
+namespace ImageGlass.Common;
+
+
+public static class Log
+{
+    /// <summary>
+    /// Logs an error message to the debug output when in debug mode.
+    /// </summary>
+    public static void Error(string message)
+    {
+#if DEBUG
+        Debug.WriteLine($"❌ {message}");
+#endif
+    }
+
+
+    /// <summary>
+    /// Logs an error message to the debug output when in debug mode.
+    /// </summary>
+    public static void Error(Exception ex)
+    {
+        Error(ex.ToString());
+    }
+
+}
