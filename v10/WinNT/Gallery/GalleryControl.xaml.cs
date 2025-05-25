fb019d4e76616cf991f1602f56a629e11277cf26
Phap Dieu Duong <ddphap@gmail.com>
2025-05-25 12:21:33 +0800

    GalleryControl: thumbnail item size

--- v10/WinNT/Gallery/GalleryControl.xaml.cs
+++ v10/WinNT/Gallery/GalleryControl.xaml.cs
@@ -3,85 +3,12 @@
 
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using System;
-using System.Collections.Generic;
 
 namespace ImageGlass.WinNT;
 
 public sealed partial class GalleryControl : UserControl
 {
-    private double ItemWidth = 0;
-    private Thickness ItemMargin = new();
-
-
-    public GalleryControl()
-    {
-        InitializeComponent();
-
-        InitializeData();
-    }
-
-
-    private void GalleryScrollViewer_ViewChanging(object sender, ScrollViewerViewChangingEventArgs e)
-    {
-        var selectedItem = GetSelectedItemFromViewport();
-        if (selectedItem is null) return;
-
-        // update corresponding rectangle with selected color
-        GalleryScrollViewer.Background = selectedItem.Background;
-    }
-
-
-    private void GalleryItem_Selected(object sender, RoutedEventArgs e)
-    {
-        if (sender is FrameworkElement fe)
-        {
-            // When the clicked item has been received, bring it to the middle of the viewport.
-            fe.StartBringIntoView(new BringIntoViewOptions()
-            {
-                VerticalAlignmentRatio = 0.5,
-                AnimationDesired = true,
-            });
-        }
-
-
-        if (sender is Button item)
-        {
-            // Update corresponding rectangle with selected color
-            GalleryScrollViewer.Background = item.Background;
-        }
-    }
-
-
-
-    private void InitializeData()
-    {
-        var colors = new List<string>()
-        {
-            "Blue",
-            //"BlueViolet",
-            //"Crimson",
-            //"DarkCyan",
-            //"DarkGoldenrod",
-            //"DarkMagenta",
-            //"DarkOliveGreen",
-            //"DarkRed",
-            //"DarkSlateBlue",
-            //"DeepPink",
-            "IndianRed",
-            "MediumSlateBlue",
-            "Maroon",
-            "MidnightBlue",
-            "Peru",
-            "SaddleBrown",
-            "SteelBlue",
-            "OrangeRed",
-            "Firebrick",
-            "DarkKhaki"
-        };
-
-    }
-
+    public int ItemSize { get; set; } = 70;
 
     public object ItemsSource
     {
@@ -93,27 +20,31 @@ public sealed partial class GalleryControl : UserControl
     }
 
 
-    // Return item that's at the center of the viewport.
-    private Button? GetSelectedItemFromViewport()
+    public GalleryControl()
     {
-        var selectedIndex = GetSelectedIndexFromViewport();
-        var selectedElement = GalleryItemRepeater.TryGetElement(selectedIndex);
-
-        return selectedElement as Button;
+        InitializeComponent();
     }
 
 
-    // Find index of the item that's at the center of the viewport
-    private int GetSelectedIndexFromViewport()
+    public void SelectItem()
     {
-        // get horizontal center point of ScrollViewer
-        var centerPoint = GalleryScrollViewer.HorizontalOffset + GalleryScrollViewer.ViewportWidth / 2;
 
-        var selectedIndex = (int)Math.Floor(centerPoint / (ItemMargin.Left + ItemWidth));
-        selectedIndex %= GalleryItemRepeater.ItemsSourceView.Count;
-
-        return selectedIndex;
     }
+
+
+    private void GalleryItem_Selected(object sender, RoutedEventArgs e)
+    {
+        if (sender is not FrameworkElement fe) return;
+
+        // When the clicked item has been received, bring it to the middle of the viewport.
+        fe.StartBringIntoView(new BringIntoViewOptions()
+        {
+            VerticalAlignmentRatio = 0.5,
+            HorizontalAlignmentRatio = 0.5,
+            AnimationDesired = true,
+        });
+    }
+
 
     private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
     {
