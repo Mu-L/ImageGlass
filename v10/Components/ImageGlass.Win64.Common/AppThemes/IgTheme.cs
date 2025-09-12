47c7d24166b116ad0bf2d2f8827f0812af01d33b
Phap Dieu Duong <ddphap@gmail.com>
2025-09-12 23:25:29 +0800

    AppThemes\: changed namespace ImageGlass.Win64.Common to ImageGlass.Common

--- v10/Components/ImageGlass.Win64.Common/AppThemes/IgTheme.cs
+++ v10/Components/ImageGlass.Win64.Common/AppThemes/IgTheme.cs
@@ -16,13 +16,12 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-using ImageGlass.Common;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json.Serialization;
 using Windows.UI;
 
-namespace ImageGlass.Win64.Common;
+namespace ImageGlass.Common;
 
 
 [JsonSerializable(typeof(IgTheme))]
--- v10/Components/ImageGlass.Win64.Common/AppThemes/IgThemeComputedColors.cs
+++ v10/Components/ImageGlass.Win64.Common/AppThemes/IgThemeComputedColors.cs
@@ -16,10 +16,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-using ImageGlass.Common;
 using Windows.UI;
 
-namespace ImageGlass.Win64.Common;
+namespace ImageGlass.Common;
 
 
 public partial class IgThemeComputedColors : IgReactive
