fb9c5f5205cf78ecc8ccefb7d79b45063e6f5b77
D2 <ddphap@gmail.com>
2026-01-17 14:51:39 +0800

    add FileSystem

---
+++ vNext/ImageGlass.Core/Common/FileSystem/FileSearcherImpl.cs
@@ -0,0 +1,306 @@
+﻿/*
+ImageGlass Project - Image viewer for Windows
+Copyright (C) 2010 - 2026 DUONG DIEU PHAP
+Project homepage: https://imageglass.org
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using ImageGlass.Common.Types;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImageGlass.Common.FileSystem;
+
+
+/// <summary>
+/// Handles file searching, filtering, and sorting based on specified criteria.
+/// </summary>
+public abstract partial class FileSearcherImpl<TOptions>() : DisposableImpl
+    where TOptions : FileSearchOptions
+{
+    protected CancellationTokenSource? _cancelSearching;
+
+
+    // Public Properties
+    #region Public Properties
+
+    /// <summary>
+    /// Gets a value indicating whether the search operation has completed.
+    /// </summary>
+    public bool IsSearchEnded { get; protected set; } = false;
+
+    #endregion // Public Properties
+
+
+
+    /// <summary>
+    /// Searches files from the provided directories with
+    /// <see cref="FindFiles(string, TOptions, IProgress{FileSearchingEventArgs}, CancellationToken)"/>.
+    /// </summary>
+    /// <param name="dirs">List of directories to search for files</param>
+    public virtual async Task SearchAsync(IEnumerable<string> dirs, TOptions options, IProgress<FileSearchingEventArgs> progress)
+    {
+        // cancel ongoing search
+        CancelSearching();
+        IsSearchEnded = false;
+
+
+        // get files from the given directories
+        await Task.Run(() =>
+        {
+            foreach (var dirPath in dirs)
+            {
+                FindFiles(dirPath, options, progress, _cancelSearching.Token);
+            }
+        });
+
+        IsSearchEnded = true;
+    }
+
+
+    /// <summary>
+    /// Cancels an ongoing file searching operation.
+    /// </summary>
+    [MemberNotNull(nameof(_cancelSearching))]
+    public void CancelSearching()
+    {
+        _cancelSearching?.Cancel();
+        _cancelSearching?.Dispose();
+        _cancelSearching = new();
+    }
+
+
+
+    // Protected Functions
+    #region Protected Functions
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnDisposing()
+    {
+        base.OnDisposing();
+
+        CancelSearching();
+    }
+
+
+    /// <summary>
+    /// Filters a collection of strings and returns the filtered results.
+    /// </summary>
+    protected virtual IEnumerable<string> OnFiltering(IEnumerable<string> fileList,
+        TOptions options)
+    {
+        if (options.AllowedExtensions is null) return fileList;
+
+        return fileList.Where(path =>
+        {
+            var ext = Path.GetExtension(path).ToLowerInvariant();
+
+            return options.AllowedExtensions.Contains(ext);
+        });
+    }
+
+
+    /// <summary>
+    /// Sorts a collection of image file paths based on provided criteria.
+    /// </summary>
+    protected virtual OrderedParallelQuery<string> OnSorting(IEnumerable<string> fileList,
+        TOptions options)
+    {
+        return SortFiles(fileList, options);
+    }
+
+
+    /// <summary>
+    /// Finds files in the given directory, emits <see cref="FileSearching"/> event.
+    /// </summary>
+    protected void FindFiles(string dirPath, TOptions options,
+        IProgress<FileSearchingEventArgs> progress, CancellationToken token)
+    {
+        // cancel if requested
+        if (token.IsCancellationRequested) return;
+
+        // check attributes to skip
+        var skipAttrs = FileAttributes.System;
+        if (!options.IncludeHidden) skipAttrs |= FileAttributes.Hidden;
+
+        // search files
+        var filePaths = Directory.EnumerateFiles(dirPath, "*", new EnumerationOptions()
+        {
+            IgnoreInaccessible = true,
+            AttributesToSkip = skipAttrs,
+            RecurseSubdirectories = options.SearchSubDirectories,
+        });
+
+
+        // cancel if requested
+        if (token.IsCancellationRequested) return;
+
+        // filter list
+        filePaths = OnFiltering(filePaths, options);
+
+
+        // cancel if requested
+        if (token.IsCancellationRequested) return;
+
+        // sort list
+        filePaths = OnSorting(filePaths, options);
+
+
+        // cancel if requested
+        if (token.IsCancellationRequested) return;
+
+        // emits results
+        progress.Report(new FileSearchingEventArgs(filePaths, IsSearchEnded));
+    }
+
+
+    /// <summary>
+    /// Sorts a collection of image file paths based on provided criteria.
+    /// </summary>
+    public static OrderedParallelQuery<string> SortFiles(IEnumerable<string> fileList, TOptions options)
+    {
+        var query = fileList.AsParallel();
+
+        // Gets the file path comparer.
+        var filePathComparer = new StringNaturalComparer(options.OrderType == ImageOrderType.Asc, StringComparison.OrdinalIgnoreCase);
+
+        // Gets the directory path comparer.
+        var dirPathComparer = options.GroupByDir
+            ? new StringNaturalComparer(options.OrderType == ImageOrderType.Asc, StringComparison.OrdinalIgnoreCase)
+            : (IComparer<string?>)Comparer<string>.Create((a, b) => 0);
+
+
+        // sort by FileSize
+        if (options.OrderBy == ImageOrderBy.FileSize)
+        {
+            if (options.OrderType == ImageOrderType.Desc)
+            {
+                return query
+                    .OrderBy(f => Path.GetDirectoryName(f), dirPathComparer)
+                    .ThenByDescending(f => new FileInfo(f).Length)
+                    .ThenBy(f => Path.GetFileName(f), filePathComparer);
+            }
+            else
+            {
+                return query
+                    .OrderBy(f => Path.GetDirectoryName(f), dirPathComparer)
+                    .ThenBy(f => new FileInfo(f).Length)
+                    .ThenBy(f => Path.GetFileName(f), filePathComparer);
+            }
+        }
+
+        // sort by DateCreated
+        if (options.OrderBy == ImageOrderBy.DateCreated)
+        {
+            if (options.OrderType == ImageOrderType.Desc)
+            {
+                return query
+                    .OrderBy(f => Path.GetDirectoryName(f), dirPathComparer)
+                    .ThenByDescending(f => new FileInfo(f).CreationTimeUtc)
+                    .ThenBy(f => Path.GetFileName(f), filePathComparer);
+            }
+            else
+            {
+                return query
+                    .OrderBy(f => Path.GetDirectoryName(f), dirPathComparer)
+                    .ThenBy(f => new FileInfo(f).CreationTimeUtc)
+                    .ThenBy(f => Path.GetFileName(f), filePathComparer);
+            }
+        }
+
+        // sort by Extension
+        if (options.OrderBy == ImageOrderBy.Extension)
+        {
+            if (options.OrderType == ImageOrderType.Desc)
+            {
+                return query
+                    .OrderBy(f => Path.GetDirectoryName(f), dirPathComparer)
+                    .ThenBy(f => new FileInfo(f).Extension, StringComparer.OrdinalIgnoreCase)
+                    .ThenBy(f => Path.GetFileName(f), filePathComparer);
+            }
+            else
+            {
+                return query
+                    .OrderBy(f => Path.GetDirectoryName(f), dirPathComparer)
+                    .ThenBy(f => new FileInfo(f).Extension, StringComparer.OrdinalIgnoreCase)
+                    .ThenBy(f => Path.GetFileName(f), filePathComparer);
+            }
+        }
+
+        // sort by DateAccessed
+        if (options.OrderBy == ImageOrderBy.DateAccessed)
+        {
+            if (options.OrderType == ImageOrderType.Desc)
+            {
+                return query
+                    .OrderBy(f => Path.GetDirectoryName(f), dirPathComparer)
+                    .ThenByDescending(f => new FileInfo(f).LastAccessTimeUtc)
+                    .ThenBy(f => Path.GetFileName(f), filePathComparer);
+            }
+            else
+            {
+                return query
+                    .OrderBy(f => Path.GetDirectoryName(f), dirPathComparer)
+                    .ThenBy(f => new FileInfo(f).LastAccessTimeUtc)
+                    .ThenBy(f => Path.GetFileName(f), filePathComparer);
+            }
+        }
+
+        // sort by DateModified
+        if (options.OrderBy == ImageOrderBy.DateModified)
+        {
+            if (options.OrderType == ImageOrderType.Desc)
+            {
+                return query
+                    .OrderBy(f => Path.GetDirectoryName(f), dirPathComparer)
+                    .ThenByDescending(f => new FileInfo(f).LastWriteTimeUtc)
+                    .ThenBy(f => Path.GetFileName(f), filePathComparer);
+            }
+            else
+            {
+                return query
+                    .OrderBy(f => Path.GetDirectoryName(f), dirPathComparer)
+                    .ThenBy(f => new FileInfo(f).LastWriteTimeUtc)
+                    .ThenBy(f => Path.GetFileName(f), filePathComparer);
+            }
+        }
+
+        // sort by Random
+        if (options.OrderBy == ImageOrderBy.Random)
+        {
+            // NOTE: ignoring the 'descending order' setting
+            return query
+                .OrderBy(f => Path.GetDirectoryName(f), dirPathComparer)
+                .ThenBy(_ => Guid.NewGuid());
+        }
+
+
+        // sort by Name (default)
+        return query
+            .OrderBy(f => Path.GetDirectoryName(f), dirPathComparer)
+            .ThenBy(f => Path.GetFileName(f), filePathComparer);
+    }
+
+    #endregion // Protected Functions
+
+
+}
---
+++ vNext/ImageGlass.Win32/_Common/FileSystem/FileSearcher.cs
@@ -0,0 +1,274 @@
+﻿/*
+ImageGlass Project - Image viewer for Windows
+Copyright (C) 2010 - 2026 DUONG DIEU PHAP
+Project homepage: https://imageglass.org
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+using D2Phap;
+using ImageGlass.Common.FileSystem;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImageGlass.Win32.Common.FileSystem;
+
+
+public partial class FileSearcher : FileSearcherImpl<FileShellSearchOptions>
+{
+
+    /// <summary>
+    /// Searches files from the provided directories.
+    /// If <see cref="FileShellSearchOptions.UseExplorerSortOrder"/> is <c>true</c>,
+    /// it follows there steps:
+    /// 
+    /// <list type="number">
+    /// <item>
+    ///   Get files from provided shell view of <see cref="FileShellSearchOptions.ForegroundShell"/>,
+    ///   ignoring the param <c><paramref name="dirs"/></c>.
+    /// </item>
+    /// <item>
+    ///   If not (or error), try to get the shell view
+    ///   from each param <c><paramref name="dirs"/></c> provided, then do step 1.
+    /// </item>
+    /// <item>
+    ///   If not shell view from step 2, use the normal searching process:
+    /// </item>
+    /// </list>
+    /// 
+    /// <inheritdoc/>
+    /// </summary>
+    public override async Task SearchAsync(IEnumerable<string> dirs, FileShellSearchOptions options, IProgress<FileSearchingEventArgs> progress)
+    {
+        // cancel ongoing search
+        CancelSearching();
+        IsSearchEnded = false;
+
+
+        // 1. get files from the foreground window
+        if (options.ForegroundShell != null && options.UseExplorerSortOrder)
+        {
+            try
+            {
+                // get shell folder
+                var folderShell = GetShellFolderView(null, options.ForegroundShell);
+
+                FindFiles_WithShell(folderShell.View, folderShell.DirPath, options, progress, _cancelSearching.Token);
+                return;
+            }
+            catch (COMException) { }
+        }
+
+
+        // 2. get files from the given directories
+        var fvMap = new ConcurrentDictionary<string, ExplorerFolderView?>();
+        var dirList = dirs.ToList();
+
+        if (options.UseExplorerSortOrder)
+        {
+            // find and save all shell folder view
+            foreach (var dirPath in dirList)
+            {
+                var fv = GetShellFolderView(dirPath, null).View;
+                _ = fvMap.TryAdd(dirPath, fv);
+            }
+        }
+
+
+        // 3. start searching in a separate thread
+        await Task.Run(() =>
+        {
+            foreach (var dirPath in dirList)
+            {
+                var folderShellView = fvMap.GetValueOrDefault(dirPath);
+
+                // with shell
+                if (folderShellView != null)
+                {
+                    FindFiles_WithShell(folderShellView, dirPath, options, progress, _cancelSearching.Token);
+                }
+
+                // without shell
+                else
+                {
+                    FindFiles(dirPath, options, progress, _cancelSearching.Token);
+                }
+            }
+        });
+
+
+        // 4. end searching
+        IsSearchEnded = true;
+
+        // dipose shell objects
+        foreach (var fv in fvMap)
+        {
+            fv.Value?.Dispose();
+        }
+        fvMap.Clear();
+    }
+
+
+    /// <summary>
+    /// Finds files in the given <see cref="ExplorerFolderView"/>.
+    /// Use the <see cref="FilesEnumerated"/> event to get results.
+    /// </summary>
+    private void FindFiles_WithShell(ExplorerFolderView? fv, string? rootDir, FileShellSearchOptions options, IProgress<FileSearchingEventArgs> progress, CancellationToken token)
+    {
+        // if no folder view
+        if (fv is null)
+        {
+            // use .NET
+            if (!string.IsNullOrWhiteSpace(rootDir))
+            {
+                FindFiles(rootDir, options, progress, token);
+            }
+            return;
+        }
+
+
+        // 1. get & filter files from shell folder view
+        var filePaths = fv.GetItems(FolderItemViewOptions.SVGIO_FLAG_VIEWORDER)
+            .Where(path =>
+            {
+                // ignore special folders
+                if (path.StartsWith(EggShell.SPECIAL_DIR_PREFIX, StringComparison.InvariantCultureIgnoreCase)) return false;
+
+                try
+                {
+                    // get path attributes
+                    var attrs = File.GetAttributes(path);
+
+                    // path is dir
+                    if (attrs.HasFlag(FileAttributes.Directory)) return false;
+
+                    // path is hidden
+                    if (!options.IncludeHidden && attrs.HasFlag(FileAttributes.Hidden)) return false;
+                }
+                catch
+                {
+                    return false;
+                }
+
+                // filter extensions
+                if (options.AllowedExtensions is not null)
+                {
+                    var ext = Path.GetExtension(path).ToLowerInvariant();
+
+                    return options.AllowedExtensions.Contains(ext);
+                }
+
+                return true;
+            });
+
+
+        // cancel if requested
+        if (token.IsCancellationRequested) return;
+
+
+        // 3. emits results
+        progress.Report(new FileSearchingEventArgs(filePaths, IsSearchEnded));
+
+
+        // cancel if requested
+        if (token.IsCancellationRequested) return;
+
+
+        // 4. search all sub-directories if root dir is not empty
+        if (options.SearchSubDirectories && !string.IsNullOrWhiteSpace(rootDir))
+        {
+            // search files for the sub dirs
+            // get sub folders
+            var subDirList = Directory.EnumerateDirectories(rootDir, "*", new EnumerationOptions()
+            {
+                IgnoreInaccessible = true,
+                AttributesToSkip = options.IncludeHidden
+                    ? FileAttributes.System
+                    : FileAttributes.System | FileAttributes.Hidden,
+                RecurseSubdirectories = false,
+            });
+
+            // find files in sub-folders
+            foreach (var dirPath in subDirList)
+            {
+                FindFiles(dirPath, options, progress, token);
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Gets the <see cref="ExplorerFolderView"/> from the given dir path.
+    /// </summary>
+    /// <remarks>🔴 NOTE: Must run on UI thread.</remarks>
+    /// <exception cref="COMException"></exception>
+    private static (ExplorerFolderView? View, string DirPath) GetShellFolderView(string? rootDir, ExplorerView? foregroundShell)
+    {
+        var folderPath = "";
+        ExplorerFolderView? folderView = null;
+        using var shell = new EggShell();
+
+
+        // if no dir path, get the explorer's folder view where the application opened from
+        if (string.IsNullOrWhiteSpace(rootDir))
+        {
+            if (foregroundShell?.GetTabFolderView() is ExplorerFolderView fv)
+            {
+                folderPath = foregroundShell.GetTabViewPath();
+                folderView = fv;
+            }
+        }
+        else if (!Path.EndsInDirectorySeparator(rootDir))
+        {
+            rootDir += Path.DirectorySeparatorChar;
+        }
+
+
+        rootDir ??= "";
+
+        // find the folder view from the opening explorer windows
+        if (folderView == null)
+        {
+            // find the explorer's folder view for each directory
+            shell.WithOpeningWindows(ev =>
+            {
+                var windowPath = ev.GetTabViewPath();
+                if (!Path.EndsInDirectorySeparator(windowPath))
+                {
+                    windowPath += Path.DirectorySeparatorChar;
+                }
+
+                // get the folder view for the input dir
+                if (rootDir.Equals(windowPath, StringComparison.InvariantCultureIgnoreCase)
+                    && ev.GetTabFolderView() is ExplorerFolderView fv)
+                {
+                    folderPath = windowPath;
+                    folderView = fv;
+                    return true;
+                }
+
+                return false;
+            }, true);
+        }
+
+
+        return (folderView, folderPath);
+    }
+
+}
