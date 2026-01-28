a10856ab299b40b5f6d7d349a10ba772a42b952a
D2 <ddphap@gmail.com>
2026-01-28 23:28:21 +0800

    added PhControl, PhToolButton

---
+++ vNext/ImageGlass.Lib/UI/BaseControls/PhControl.cs
@@ -0,0 +1,82 @@
+﻿/*
+ImageGlass Project - Image viewer for Windows
+Copyright (C) 2010 - 2026 DUONG DIEU PHAP
+Project homepage: https://imageglass.org
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using ImageGlass.Common;
+using System;
+
+namespace ImageGlass.UI;
+
+public partial class PhControl : ContentControl
+{
+    protected override Type StyleKeyOverride => typeof(ContentControl);
+
+
+    #region Control Events
+
+    protected override void OnLoaded(RoutedEventArgs e)
+    {
+        base.OnLoaded(e);
+
+        OnIgLanguageChanged();
+        Core.ThemeChanged += Core_ThemeChanged;
+        Core.LanguageChanged += Core_LanguageChanged;
+    }
+
+
+    protected override void OnUnloaded(RoutedEventArgs e)
+    {
+        base.OnUnloaded(e);
+
+        Core.ThemeChanged -= Core_ThemeChanged;
+        Core.LanguageChanged -= Core_LanguageChanged;
+    }
+
+
+    private void Core_ThemeChanged(object? sender, ThemePackChangedEventArgs e)
+    {
+        OnIgThemeChanged(e);
+    }
+
+
+    private void Core_LanguageChanged(object? sender, EventArgs e)
+    {
+        OnIgLanguageChanged();
+    }
+
+    #endregion // Control Events
+
+
+    #region Virtual Methods
+
+    /// <summary>
+    /// Occurs when the app theme is changed.
+    /// </summary>
+    protected virtual void OnIgThemeChanged(ThemePackChangedEventArgs e) { }
+
+
+    /// <summary>
+    /// Occurs when the app language is changed.
+    /// </summary>
+    protected virtual void OnIgLanguageChanged() { }
+
+    #endregion // Virtual Methods
+
+
+}
---
+++ vNext/ImageGlass.Lib/UI/BaseControls/PhToolButton.cs
@@ -0,0 +1,219 @@
+﻿/*
+ImageGlass Project - Image viewer for Windows
+Copyright (C) 2010 - 2026 DUONG DIEU PHAP
+Project homepage: https://imageglass.org
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+using ImageGlass.Common;
+using ImageGlass.UI.Windowing;
+using System;
+using System.ComponentModel;
+
+namespace ImageGlass.UI;
+
+public partial class PhToolButton : ToggleButton
+{
+    protected override Type StyleKeyOverride => typeof(ToggleButton);
+
+
+    #region Public Properties
+
+    /// <summary>
+    /// Gets, sets the value indicates that the button is checked on click.
+    /// </summary>
+    public bool IsCheckOnClick
+    {
+        get => GetValue(IsCheckOnClickProperty);
+        set => SetValue(IsCheckOnClickProperty, value);
+    }
+    public static readonly StyledProperty<bool> IsCheckOnClickProperty =
+        AvaloniaProperty.Register<ModalWindow, bool>(nameof(IsCheckOnClick));
+
+
+    /// <summary>
+    /// Gets, sets the value indicates that the dropdown menu is open on click.
+    /// </summary>
+    public bool IsOpenDropdownMenuOnClick
+    {
+        get => GetValue(IsOpenDropdownMenuOnClickProperty);
+        set => SetValue(IsOpenDropdownMenuOnClickProperty, value);
+    }
+    public static readonly StyledProperty<bool> IsOpenDropdownMenuOnClickProperty =
+        AvaloniaProperty.Register<ModalWindow, bool>(nameof(IsOpenDropdownMenuOnClick));
+
+
+    /// <summary>
+    /// Gets, sets the dropdown menu of this button.
+    /// </summary>
+    public ContextMenu? DropdownMenu
+    {
+        get => GetValue(DropdownMenuProperty);
+        set
+        {
+            DropdownMenu?.Opening -= DropdownMenu_Opening;
+            DropdownMenu?.Opened -= DropdownMenu_Opened;
+            DropdownMenu?.Closing -= DropdownMenu_Closing;
+            DropdownMenu?.Closed -= DropdownMenu_Closed;
+
+            SetValue(DropdownMenuProperty, value);
+        }
+    }
+    public static readonly StyledProperty<ContextMenu?> DropdownMenuProperty =
+        AvaloniaProperty.Register<ModalWindow, ContextMenu?>(nameof(DropdownMenu));
+
+
+    #endregion // Public Properties
+
+
+
+    #region Control Events
+
+    protected override void OnLoaded(RoutedEventArgs e)
+    {
+        base.OnLoaded(e);
+
+        OnIgLanguageChanged();
+        Core.ThemeChanged += Core_ThemeChanged;
+        Core.LanguageChanged += Core_LanguageChanged;
+    }
+
+
+    protected override void OnUnloaded(RoutedEventArgs e)
+    {
+        base.OnUnloaded(e);
+
+        Core.ThemeChanged -= Core_ThemeChanged;
+        Core.LanguageChanged -= Core_LanguageChanged;
+    }
+
+
+    protected override void OnClick()
+    {
+        if (IsOpenDropdownMenuOnClick && DropdownMenu is not null)
+        {
+            DropdownMenu.Open(this);
+        }
+
+        base.OnClick();
+    }
+
+
+    protected override void Toggle()
+    {
+        // don't call Toggle() method if not allowed.
+        if (!IsCheckOnClick) return;
+
+        base.Toggle();
+    }
+
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs e)
+    {
+        base.OnPropertyChanged(e);
+
+        if (e.Property == DropdownMenuProperty)
+        {
+            DropdownMenu?.Opening += DropdownMenu_Opening;
+            DropdownMenu?.Opened += DropdownMenu_Opened;
+            DropdownMenu?.Closing += DropdownMenu_Closing;
+            DropdownMenu?.Closed += DropdownMenu_Closed;
+        }
+    }
+
+
+    private void DropdownMenu_Opening(object? sender, CancelEventArgs e)
+    {
+        OnIgDropdownMenuOpening(e);
+    }
+
+
+    private void DropdownMenu_Opened(object? sender, RoutedEventArgs e)
+    {
+        OnIgDropdownMenuOpened(e);
+    }
+
+
+    private void DropdownMenu_Closing(object? sender, CancelEventArgs e)
+    {
+        OnIgDropdownMenuClosing(e);
+    }
+
+
+    private void DropdownMenu_Closed(object? sender, RoutedEventArgs e)
+    {
+        OnIgDropdownMenuClosed(e);
+    }
+
+
+    private void Core_ThemeChanged(object? sender, ThemePackChangedEventArgs e)
+    {
+        OnIgThemeChanged(e);
+    }
+
+
+    private void Core_LanguageChanged(object? sender, EventArgs e)
+    {
+        OnIgLanguageChanged();
+    }
+
+    #endregion // Control Events
+
+
+
+    #region Virtual Methods
+
+    /// <summary>
+    /// Occurs when the app theme is changed.
+    /// </summary>
+    protected virtual void OnIgThemeChanged(ThemePackChangedEventArgs e) { }
+
+
+    /// <summary>
+    /// Occurs when the app language is changed.
+    /// </summary>
+    protected virtual void OnIgLanguageChanged() { }
+
+
+    /// <summary>
+    /// Occurs when the value of the IsOpen property is changing from false to true.
+    /// </summary>
+    protected virtual void OnIgDropdownMenuOpening(CancelEventArgs e) { }
+
+
+    /// <summary>
+    /// Occurs when the dropdown menu is opened.
+    /// </summary>
+    protected virtual void OnIgDropdownMenuOpened(RoutedEventArgs e) { }
+
+
+    /// <summary>
+    /// Occurs when the value of the IsOpen property is changing from true to false.
+    /// </summary>
+    protected virtual void OnIgDropdownMenuClosing(CancelEventArgs e) { }
+
+
+    /// <summary>
+    /// Occurs when the dropdown menu is closed.
+    /// </summary>
+    protected virtual void OnIgDropdownMenuClosed(RoutedEventArgs e) { }
+
+    #endregion // Virtual Methods
+
+
+}
